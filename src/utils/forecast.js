2814b9c88fc92bcbfcaffa8491b92c0e9359eb94
Jeehad Jebeile <jjebeile@adminsmacintosh.lan>
2020-01-26 14:50:05 +1100

    Added temperature max and min to weather details

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,9 +9,9 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' High: ' + body.daily.data[0].temperatureMax + ' Low: ' + body.daily.data[0].temperatureMin + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain.')
         }
     })
 }
-
+// High and low values.
 module.exports = forecast
